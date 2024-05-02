7d26705fe68fe56160542df993e520e421a48f56
Mikhail <mbold1911@yandex.ru>
2024-05-02 16:50:21 +0300

    init

---
+++ travel/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+# export DJANGO_SETTINGS_MODULE=travel.settings.dev
+
+DEBUG = True
+print("#"*20 + ' DEBUG=True')
+
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
---
+++ travel/settings/prod.py
@@ -0,0 +1,13 @@
+import os
+from .base import *
+# export DJANGO_SETTINGS_MODULE=travel.settings.prod
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+
+ADMINS = [
+    ('Mikhail B.', 'haxboxmiha@gmail.com'),
+]
